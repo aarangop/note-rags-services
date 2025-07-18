7063a4ee3667404f463972e5b6c77a2240fd3e59
Andres Arango <andresap@proton.me>
2025-07-18 18:06:05 -0500

    Basic note editor

--- frontend/src/routes/notes/+page.svelte
+++ frontend/src/routes/notes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import MarkdownEditor from '$lib/components/MarkdownEditor.svelte';
+	import NoteEditor from '$lib/components/NoteEditor.svelte';
 </script>
 
 <svelte:head>
@@ -8,7 +8,5 @@
 </svelte:head>
 
 <div class="mx-auto flex h-screen max-w-4xl flex-col px-4 pt-4 pb-4 sm:px-6 lg:px-8">
-	<MarkdownEditor
-		value="Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
-	/>
+	<NoteEditor fileName="Sample File"></NoteEditor>
 </div>
